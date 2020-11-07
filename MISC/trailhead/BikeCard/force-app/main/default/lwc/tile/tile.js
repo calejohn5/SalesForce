08d1b9aec509652b47c2468c3d6fdfa16d060bee
caleb <calebnj@live.com>
2020-11-06 18:57:15 -0600

    finish

--- MISC/trailhead/BikeCard/force-app/main/default/lwc/tile/tile.js
+++ MISC/trailhead/BikeCard/force-app/main/default/lwc/tile/tile.js
@@ -2,13 +2,13 @@ import { LightningElement, api } from 'lwc';
 
 export default class Tile extends LightningElement {
     @api product;
-
+// field is clicked on html, calling this function then sent out
     tileClick() {
         const event = new CustomEvent('tileclick', {
-            // detail contains only primitives
+// detail contains only primitives
             detail: this.product.fields.Id.value
         });
-        // Fire the event from c-tile
+// Fire the event from c-tile
         this.dispatchEvent(event);
     }
 }
